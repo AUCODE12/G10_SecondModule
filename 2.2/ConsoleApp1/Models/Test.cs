e088d8e164ed3a7007d4bad546126f4d38e3c9ed
Ahmadjon Qudratov <120676674+AUCODE12@users.noreply.github.com>
2024-12-05 12:24:20 +0500

    Update

---
+++ 2.2/ConsoleApp1/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1.Models;
+
+public class Test
+{
+    public Guid Id { get; set; }
+
+    public string Question { get; set; }
+
+    public string AVariant { get; set; }
+
+    public string BVariant { get; set; }
+
+    public string CVariant { get; set; }
+
+    public string Answer { get; set; }
+}
---
+++ 2.2/ConsoleApp1/Services/TestService.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp1.Models;
+using System.Text.Json;
+
+namespace ConsoleApp1.Services;
+
+public class TestService
+{
+    private string testFilePath;
+
+    public TestService()
+    {
+        testFilePath = "../../../Data/Tests.json";
+        if (File.Exists(testFilePath) is false)
+        {
+            File.WriteAllText(testFilePath, "[]");
+        }
+    }
+
+    private void SaveData(List<Test> tests)
+    {
+        var testsJson = JsonSerializer.Serialize(tests);
+        File.WriteAllText(testFilePath, testsJson);
+    }
+
+    private List<Test> GetTests()
+    {
+        var testsJson = File.ReadAllText(testFilePath);
+        var tests = JsonSerializer.Deserialize<List<Test>>(testsJson);
+
+        return tests;
+    }
+}

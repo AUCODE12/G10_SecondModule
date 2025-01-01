a5618aecc8b04a1ab7b6eefcf4ac48b4c246c0c6
Ahmadjon Qudratov <120676674+AUCODE12@users.noreply.github.com>
2025-01-01 15:28:05 +0500

    Update

---
+++ 2.11/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Queue queue1 = new Queue();
+
+        queue1.Enqueue(new test());
+        Console.WriteLine(queue1.Dequeue());
+    }
+}

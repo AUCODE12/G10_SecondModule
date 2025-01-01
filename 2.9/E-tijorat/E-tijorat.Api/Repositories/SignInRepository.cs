fc8aea82b7b0dec5c1c875fb48160877cf37236b
Ahmadjon Qudratov <120676674+AUCODE12@users.noreply.github.com>
2025-01-01 16:16:28 +0500

    Update

--- 2.9/E-tijorat/E-tijorat.Api/Repositories/SignInRepository.cs
+++ 2.9/E-tijorat/E-tijorat.Api/Repositories/SignInRepository.cs
@@ -1,5 +1,17 @@
-﻿namespace E_tijorat.Api.Repositories;
+﻿using E_tijorat.Api.DataAccess.Entities;
+
+namespace E_tijorat.Api.Repositories;
 
 public class SignInRepository : ISignInRepository
 {
+
+    public void RemoveSignIp(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Guid WriteSignIp(ETijoratSignIn signUp)
+    {
+        throw new NotImplementedException();
+    }
 }
--- 2.9/E-tijorat/E-tijorat.Api/Repositories/SignUpRepository.cs
+++ 2.9/E-tijorat/E-tijorat.Api/Repositories/SignUpRepository.cs
@@ -1,7 +1,42 @@
-﻿namespace E_tijorat.Api.Repositories;
+﻿using E_tijorat.Api.DataAccess.Entities;
+using System.Text.Json;
+
+namespace E_tijorat.Api.Repositories;
 
 public class SignUpRepository : ISignUpRepository
 
 {
+    private readonly string _signUpPath;
+    private readonly List<ETijoratSignUp> _eTijoratSignUps;
 
+    public SignUpRepository()
+    {
+        _signUpPath = "../../../DataAccess/Data/SignUpUsers.json";
+        if (!File.Exists(_signUpPath))
+        {
+            File.WriteAllText(_signUpPath, "[]");
+        }
+
+        _eTijoratSignUps = ReadAllSignUpUser();
+    }
+
+    public Guid WriteSignUp(ETijoratSignUp signUp)
+    {
+        _eTijoratSignUps.Add(signUp);
+        SaveData();
+        return signUp.Id;
+    }
+
+    private void SaveData()
+    {
+        var signUpJson = JsonSerializer.Serialize(_eTijoratSignUps);
+        File.WriteAllText(signUpJson, signUpJson);
+    }
+
+    private List<ETijoratSignUp> ReadAllSignUpUser()
+    {
+        var signUpJson = File.ReadAllText(_signUpPath);
+        var signUpList = JsonSerializer.Deserialize<List<ETijoratSignUp>>(signUpJson); 
+        return signUpList;
+    }
 }
